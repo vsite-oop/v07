bb3d5626033b42dee1120b08aa4a3f2a9b1a5462
Filip Lončar <filoncar@vsite.hr>
2022-12-05 21:16:43 +0100

    Native Unit Test Project added + settings

---
+++ test/test.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../app/money.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace vsite::oop::v7;
+
+namespace test
+{
+	TEST_CLASS(test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}

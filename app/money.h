2a0c8e185fd356b0ce9f0c6647188d90d222f321
Filip Lončar <filoncar@vsite.hr>
2023-01-29 20:43:53 +0100

    relese build on git test

--- app/money.h
+++ app/money.h
@@ -5,22 +5,22 @@
 namespace vsite::oop::v7 
 {
 
-class money {
-private:
-	int kn;
-	int lp;
+	class money {
+	private:
+		int kn;
+		int lp;
 
-public:
-	money(int kn = 0, int lp = 0);
+	public:
+		money(int kn = 0, int lp = 0);
 
-	friend std::istream& operator>> (std::istream&, money&);
-	friend std::ostream& operator<< (std::ostream&, const money&);
+		friend std::istream& operator>> (std::istream&, money&);
+		friend std::ostream& operator<< (std::ostream&, const money&);
 
-	money& operator+= (const money&);
-	money& operator-= (const money&);
+		money& operator+= (const money&);
+		money& operator-= (const money&);
 
-	int kune() const;
-	int lipe() const;
-};
+		int kune() const;
+		int lipe() const;
+	};
 
 }

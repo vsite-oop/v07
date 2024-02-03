71be14bfb2938fb9b4cbad94fbb574c4953800d4
Fran <fran.habijanic@gmail.com>
2024-02-03 21:45:05 +0100

    Vj7

--- app/money.h
+++ app/money.h
@@ -1,8 +1,27 @@
-#pragma once
+﻿﻿#pragma once
+#include <iostream>
 
-namespace vsite::oop::v7 
+namespace vsite::oop::v7
 {
 
-class money {};
+	class money {
+		int total;
+
+	public:
+		// konstruktori
+		money(int e = 0, int c = 0);
+
+		// operatori
+		money& operator +=(const money& m);
+		money& operator -=(const money& m);
+
+		friend std::istream& operator >> (std::istream& is, money& m);
+		friend std::ostream& operator << (std::ostream& os, const money& m);
+
+		// metode
+		int get_value() const;
+		void add_value(int addt, int dec);
+		void sub_value(int subs, int dec);
+	};
 
 }
